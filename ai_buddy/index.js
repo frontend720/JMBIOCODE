2908ba41d42f7a1df213b32a3cb7741da49b5486
frontend720 <109117216+frontend720@users.noreply.github.com>
2024-02-07 18:22:32 -0700

    edited routes

--- ai_buddy/index.js
+++ ai_buddy/index.js
@@ -10,6 +10,8 @@ app.use(express.json())
 app.use(morgan("dev"))
 
 app.use("/", require("./studyRouter"))
+app.use("/notes", require("./noteRouter"))
+app.use("/account", require("./accountRouter"))
 
 
 app.listen(port, () => {
--- ai_buddy/studyRouter.js
+++ ai_buddy/studyRouter.js
@@ -2,28 +2,26 @@ const express = require("express");
 const studyRouter = express.Router();
 const { openai, db } = require("./config");
 const { v4: uuid } = require("uuid");
+const moment = require("moment");
 
-studyRouter.post("/question", (req, res) => {
-  const collectionRef = db
-    .collection("user")
-    .doc(req.body.user)
-    .collection("question")
-    .doc(uuid())
-    .set({
-      question: req.body.question,
-    });
+studyRouter.post("/question/:id", (req, res) => {
+  const firestoreData = [];
+  let conversationData;
+  let conversationQuestion;
+  const id = req.params.id;
+
 
   const data = {
     messages: [
       {
         role: "system",
-        content: `You are a educational tutor named ${req.body.tutor}, your job is to, in a casual, instructional format explain a given subject at an ${req.body.educational} level to ${req.body.user} in about 1400 tokens or less. When possible use data from Wikipedia.com, Dictionary.com and Google Scholar`,
+        content: `You are an educational tutor named ${req.body.tutor}, your job is to, in a casual, instructional format explain a given subject at an ${req.body.educational} level to ${req.body.user} in about 1400 tokens or less. When possible use data from Wikipedia.com, Dictionary.com and Google Scholar`,
       },
       { role: "user", content: req.body.question },
     ],
     model: "gpt-3.5-turbo",
     temperature: 0.4,
-    max_tokens: 800,
+    max_tokens: 1400,
   };
   const chatCompletion = openai.chat.completions.create(data);
 
@@ -32,24 +30,67 @@ studyRouter.post("/question", (req, res) => {
       if (!completion.choices) {
         res
           .status(404)
-          .send({ message: "No results to display, try another search." });
+          .send({ message: "Error finding answer. Try again later." });
       } else {
-        res.status(200).send(completion.choices[0]);
+          conversationData = completion.choices[0].message.content;
+          conversationQuestion = req.body.question;
+          res.status(200).send(conversationData)
       }
-      collectionRef
-        .then((collect) => {
-          if (!collect.writeTime)
-            res.status(400).send({ message: "This is an error" });
-          else {
-            res.status(200).send(completion.choices[0]);
-          }
-        })
-        .catch((error) => {
-          res.status(500).send({ message: "Error: " + error.message });
-        });
     })
-    .catch((error) => {
-      res.status(500).send({ message: "Error: " + error.message });
+    .catch((err) => {
+      res.status(500).send({ message: err.code });
+    });
+});
+
+studyRouter.put("/:id", (req, res) => {
+  const id = req.params.id;
+
+  const data = {
+    question: req.body.question,
+    editedTimestamp: moment().format("llll"),
+    edited: true,
+  };
+  const collectionRef = db
+    .collection("users")
+    .doc(req.body.user)
+    .collection("question")
+    .doc(id)
+    .set(data, { merge: true });
+  collectionRef
+    .then((question) => {
+      if (!question) {
+        res
+          .status(400)
+          .send({ message: "Error editing question. Try again later." });
+      } else {
+        res.status(200).send(question);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err.code });
+    });
+});
+
+studyRouter.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  const collectionRef = db
+    .collection("users")
+    .doc(req.body.user)
+    .collection("question")
+    .doc(id)
+    .delete();
+  collectionRef
+    .then((question) => {
+      if (!question) {
+        res
+          .status(400)
+          .send({ message: "Error deleting question. Try again later." });
+      } else {
+        res.status(200).send(question);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err.code });
     });
 });
 
